42a791cdae3049ef72350be6b6818ca8d7e75fe4
Tu Nguyen <hikaruyh@gmail.com>
2013-09-10 18:12:33 -0400

    auto

--- ComputingLogic/Practice/3.11.pl
+++ ComputingLogic/Practice/3.11.pl
@@ -1,13 +1,13 @@
 %% flatten( List, Flatlist)
 
-conc([],L,L).
-conc([X|L1],L2,[X|L3]):-
-conc(L1,L2,L3).
+flatten(List, FlatList) :-
+    flatten(List, [], FlatList0), !,
+    FlatList = FlatList0.
 
-flatten([],[]).
-flatten([X],X).
-flatten(X,X).
-flatten([X|Tail],L):-
-flatten(X,L1),
-flatten(Tail,L2),
-conc(L1,L2,L).
+flatten(Var, Tl, [Var|Tl]) :-
+    var(Var), !.
+flatten([], Tl, Tl) :- !.
+flatten([Hd|Tl], Tail, List) :- !,
+    flatten(Hd, FlatHeadTail, List),
+    flatten(Tl, Tail, FlatHeadTail).
+flatten(NonList, Tl, [NonList|Tl]).
