2cf82dc1a935fb8c23f45ea8eb42fc3dc64fe444
Tu Nguyen <hikaruyh@gmail.com>
2013-09-10 16:59:51 -0400

    auto

--- ComputingLogic/Practice/3.11.pl
+++ ComputingLogic/Practice/3.11.pl
@@ -1,9 +1,13 @@
-%% dividelist( List, List1, List2)
-%% so that the elements of List are partitioned between List1 and List2, and
-%% List1 and List2 are of approximately the same length. For example,
-%% partition( [a,b,c,d,e], [a,c,e], [b,d] ).
+%% flatten( List, Flatlist)
 
-dividelist([],[],[]).
-dividelist(X,X,[]).
-dividelist([X,Y|Tail],[X|Lx],[Y|Ly]):-
-dividelist(Tail,Lx,Ly).
+conc([],L,L).
+conc([X|L1],L2,[X|L3]):-
+conc(L1,L2,L3).
+
+flatten([],[]).
+flatten([X],X).
+flatten(X,X).
+flatten([X|Tail],L):-
+flatten(X,L1),
+flatten(Tail,L2),
+conc(L1,L2,L).
