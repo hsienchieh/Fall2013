011623e4beb4c86446456255e23d38cb303d642b
Tu Nguyen <hikaruyh@gmail.com>
2013-09-14 21:40:05 -0400

    auto

--- ComputingLogic/Practice/homework01.pl
+++ ComputingLogic/Practice/homework01.pl
@@ -1,6 +1,7 @@
 %% Problem01
 
 conc([],L,L).
+
 conc([X|L1],L2,[X|L3]):-
 	conc(L1,L2,L3).
 
@@ -12,31 +13,37 @@ interleave(X,Y,Z):-
 	findinterleave(X,Y,Z).
 
 findinterleave([], Xs, Xs).
+
 findinterleave([X|Xs], [Y|Ys], [X,Y|Zs]) :- 
 	findinterleave(Xs, Ys, Zs).
+	
 findinterleave([X|Xs], [Y|Ys], [Y,X|Zs]) :- 
 	findinterleave(Xs, Ys, Zs).
 
 %% Problem02
-genprimes(N,L) :- 
-   p_list(1,N,L).
 
-p_list(A,B,[]) :- 
-   A > B, !.
-p_list(A,B,[A|L]) :- 
+genprimes(N,[]):-
+	N < 2, !.
+genprimes(N,[2|L]) :- 
+   listprime(1,N,L).
+
+listprime(A,B,[]) :- 
+    A > B, !.
+	
+listprime(A,B,[A|L]) :- 
     isprime(A), !, 
     A1 is A + 2, 
-    p_list(A1,B,L).
-
-p_list(A,B,L) :- 
+    listprime(A1,B,L).
+	
+listprime(A,B,L) :- 
     A1 is A + 2, 
-    p_list(A1,B,L).
+    listprime(A1,B,L).
 	
 divisible(X,Y):-
 	N is Y*Y,
 	N =< X,
 	X mod Y =:= 0.
-
+	
 divisible(X,Y):-
 	Y < X,
 	Y1 is Y+1,
@@ -46,6 +53,53 @@ isprime(X):-
 	Y is 2, 
 	X > 1, 
 	\+divisible(X,Y).
+	
+%% Problem03
+
+flatten([], []) :- !.
+flatten([L|Ls], F) :-
+    !,
+    flatten(L, L1),
+    flatten(Ls, Ls1),
+    conc(L1, Ls1, F).
+flatten(L, [L]).
+
+%% Problem04*
+
+item(a, 5).
+item(b, 3).
+item(c, 7).
+
+block(0,[]).
+
+block(N,[H]) :-
+    item(H,Nh),
+    D is N - Nh,
+    D >= 0.
+
+block(N,[H|T]) :-
+    item(H,Nh),
+    D is N - Nh,
+    D >= 0,
+	block(D,T).
+	
+%% Problem05
+%%E -> T '+' E
+%%E -> T
+%%T -> F '*' T
+%%T -> F
+%%F -> '(' E ')'
+%%F -> integer
+
+eval([],[]).
+eval([A,'+'|B], R) :- R is eval(A)+eval(B).
+eval([A,'*'|B], R) :- R is eval(A)*eval(B).
+eval(['(',A,')'],R) :- R is eval(A).
+eval(A,A).
+
+
+
+	
 
 
 
--- ComputingLogic/Practice/sample.pl
+++ ComputingLogic/Practice/sample.pl
@@ -1,31 +1,14 @@
-is_prime(2).
-is_prime(3).
-is_prime(P) :- 
-    integer(P), 
-    P > 3, 
-    P mod 2 =\= 0, 
-    \+ has_factor(P,3).  
+flatten([], []) :- !.
+flatten([L|Ls], F) :-
+    !,
+    flatten(L, L1),
+    flatten(Ls, Ls1),
+    conc(L1, Ls1, F).
+flatten(L, [L]).
 
-has_factor(N,L) :- N mod L =:= 0.
-has_factor(N,L) :- 
-    L * L < N, 
-    L2 is L + 2, 
-    has_factor(N,L2).
+conc([],L,L).
 
-prime_list(A,B,L) :- 
-    A1 is (A // 2) * 2 + 1, 
-    p_list(A1,B,L).
-
-
-p_list(A,B,[]) :- A > B, !.
-p_list(A,B,[A|L]) :- 
-    is_prime(A), !, 
-    next(A,A1), 
-    p_list(A1,B,L).
-
-p_list(A,B,L) :- 
-    next(A,A1), 
-    p_list(A1,B,L).
-
-next(2,3) :- !.
-next(A,A1) :- A1 is A + 2.
+conc([X|L1],L2,[X|L3]):-
+	conc(L1,L2,L3).
+	
+%% flatten([[[1,2],[3,4]],5,[6,[7,8]]], L2)
